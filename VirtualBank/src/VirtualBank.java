e9bf67e84d9a50ddf7ba64af2ed9637167eb85c7
Paulo "Oliveira" Ricardo <134083839+olive1rax@users.noreply.github.com>
2023-06-12 10:59:42 -0300

    Add files via upload

---
+++ VirtualBank/src/VirtualBank.java
@@ -0,0 +1,14 @@
+public class VirtualBank {
+    public static void main(String[] args) {
+        String name = "Kotlin";
+        String tipoConta = "Corrente";
+        double saldo = 1750.00;
+
+        System.out.println("****************");
+        System.out.println("\nNome: " + name);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("\n****************");
+
+    }
+}
