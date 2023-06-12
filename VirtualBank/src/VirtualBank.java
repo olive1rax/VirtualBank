ffd5797819bd41856ffb4190aea8f73c846e064f
olive1rax <contato@olive1ra.xyz>
2023-06-12 12:32:12 -0300

    Leitura de opção

--- VirtualBank/src/VirtualBank.java
+++ VirtualBank/src/VirtualBank.java
@@ -1,8 +1,11 @@
+import java.util.scanner;
+
 public class VirtualBank {
     public static void main(String[] args) {
         String name = "Kotlin";
         String tipoConta = "Corrente";
         double saldo = 1750.00;
+        int opcao;
 
         System.out.println("****************");
         System.out.println("\nNome: " + name);
@@ -10,5 +13,18 @@ public class VirtualBank {
         System.out.println("Saldo: " + saldo);
         System.out.println("\n****************");
 
+        String menu = """
+                1 - Consultar saldo.
+                2 - Transferência.
+                3 - Depositar Valor.
+                4 - Sair.
+                """
+        Scanner leitura = new Scanner(System.in);
+
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
     }
 }
